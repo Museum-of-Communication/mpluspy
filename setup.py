e6a8ab4fc9f6654e465222e1ee9cb614d9993964
Nicolas Kessler <mail@nicolaskessler.ch>
2025-02-21 22:22:31 +0100

    switched to poetry

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="mpluspy",
-    version="1.0.0",
-    description="Client for Zetcom's Museum Plus API",
-    packages=find_packages(),  # Automatically finds all packages
-    install_requires=["requests", "pyaml", "xmltodict"],
-)
