7da3aa4d55b0390fdb2fffb88b15517b6f1f8a4e
Nicolas Kessler <mail@nicolaskessler.ch>
2025-02-21 22:22:31 +0100

    more testing of MPlusClient

--- tests/test_mplusclient.py
+++ tests/test_mplusclient.py
@@ -9,14 +9,26 @@ from mpluspy import MPlusClient, MPlusResponse
 def mock_config():
     return {
         "baseurl": "https://example.zetcom.com/MpWeb/",
-        "example-request": {
+        "example-request-1": {
             "type": "POST",
-            "url": "ria-ws/application/module/Multimedia/search",
+            "url": "ria-ws/application/example",
             "headers": {
                 "Content-Type": "application/xml",
                 "Accept": "application/xml"
             },
-            "xml-body": "config/MultimediaSearch.xml"
+            "xml-body": "example-1.xml"
+        },
+        "example-request-2": {
+            "type": "POST",
+            "url": "ria-ws/application/example/{id}",
+            "headers": {},
+            "xml-body": "example-2.xml"
+        },
+        "example-request-3": {
+            "type": "POST",
+            "url": "ria-ws/application/example",
+            "headers": {},
+            "xml-body": "config/invalid.xml"
         }
     }
 
@@ -39,26 +51,75 @@ def mock_request():
 
 @pytest.fixture
 def mock_xml_file():
-    with patch("builtins.open", mock_open(read_data="<xml>Mock XML Content</xml>")):
-        yield
+
+    mock_xml = {
+        "example-1.xml": """<application xmlns="http://www.zetcom.com/ria/ws/module/search">
+                            <modules>
+                                <module name="Multimedia">
+                                    <search">
+                                    </search>
+                                </module>
+                            </modules>
+                        </application>
+                        """,
+        "example-2.xml": """<application xmlns="http://www.zetcom.com/ria/ws/module/search">
+                            <modules>
+                                <module name="Multimedia">
+                                    <search limit="{limit}" offset="{offset}">
+                                    </search>
+                                </module>
+                            </modules>
+                        </application>
+                        """,
+    }
+    
+    # Create a mock that intercepts the open call and returns the appropriate content
+    def mock_open_xml(file, mode):
+        if file in mock_xml:
+            return mock_open(read_data=mock_xml[file]).return_value
+        else:
+            raise FileNotFoundError(f"File {file} not found")
+        
+    with patch("builtins.open", mock_open_xml):
+        yield mock_xml
 
 def test_request_success(mock_mplus_client, mock_request, mock_response, mock_xml_file):
     mock_request.return_value = mock_response
-    response = mock_mplus_client.request("example-request")
+    response = mock_mplus_client.request("example-request-1")
     assert isinstance(response, MPlusResponse)
     assert response.status_code == 200
     assert "Success" in response.text
 
-def test_request_invalid_key(mock_mplus_client):
+def test_request_invalid(mock_mplus_client):
     with pytest.raises(KeyError):
         mock_mplus_client.request("invalid-request")
 
 def test_request_with_placeholders(mock_mplus_client, mock_request, mock_response, mock_xml_file):
     mock_request.return_value = mock_response
     url_placeholders = {"id": 123}
-    response = mock_mplus_client.request("example-request", url_placeholders=url_placeholders)
+    xml_placeholders = {"limit": 10, "offset": 0}
+    response = mock_mplus_client.request("example-request-2", xml_placeholders=xml_placeholders, url_placeholders=url_placeholders)
     assert isinstance(response, MPlusResponse)
     assert response.status_code == 200
+
+def test_request_with_invalid_url_placeholder(mock_mplus_client, mock_xml_file):
+    url_placeholders = {"foo": "bar"}
+    xml_placeholders = {"limit": "10", "offset": "0"}
+    with pytest.raises(KeyError):
+        mock_mplus_client.request("example-request-2", xml_placeholders=xml_placeholders, url_placeholders=url_placeholders)
+
+
+def test_request_with_invalid_xml_placeholder(mock_mplus_client, mock_xml_file):
+    url_placeholders = {"id": 123}
+    xml_placeholders = {"foo": "bar"}
+    with pytest.raises(KeyError):
+        mock_mplus_client.request("example-request-2", xml_placeholders=xml_placeholders, url_placeholders=url_placeholders)
+
+
+def test_request_with_invalid_xml_file(mock_mplus_client, mock_xml_file):
+    mock_request.return_value = mock_response
+    with pytest.raises(FileNotFoundError):
+        mock_mplus_client.request("example-request-3")
 
 def test_format_timestamp():
     timestamp = datetime(2023, 5, 17, 12, 30, 45, 123456)
@@ -66,5 +127,16 @@ def test_format_timestamp():
     assert formatted == "2023-05-17T12:30:45.123Z"
 
 def test_request_url(mock_mplus_client):
-    url = mock_mplus_client._MPlusClient__request_url("example-request", placeholders={})
-    assert url == "https://example.zetcom.com/MpWeb/ria-ws/application/module/Multimedia/search"
+    url = mock_mplus_client._MPlusClient__request_url("example-request-1")
+    assert url == "https://example.zetcom.com/MpWeb/ria-ws/application/example"
+
+    url = mock_mplus_client._MPlusClient__request_url("example-request-1", placeholders={'key': 'value'})
+    assert url == "https://example.zetcom.com/MpWeb/ria-ws/application/example"
+
+def test_request_url_with_placeholders(mock_mplus_client):
+    url = mock_mplus_client._MPlusClient__request_url("example-request-2", placeholders={'id': '123'})
+    assert url == "https://example.zetcom.com/MpWeb/ria-ws/application/example/123"
+
+def test_request_url_missing_placeholders(mock_mplus_client):
+    with pytest.raises(KeyError):
+        mock_mplus_client._MPlusClient__request_url("example-request-2", placeholders={'key': 'value'})
